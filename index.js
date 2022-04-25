eb295656379589bc7f7691a0d8001f0b70f4f678
cptknvckles <cptknvckles@protonmail.com>
2022-04-24 23:36:36 -0500

    added svg folder, stlying is complete, JS is next

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+//ok so we need an array that holds all the characters
+//make the button(function) generate 4 random passwords
+//display each password in its respective input box
+//set the password length(extra)
+//1 click copy(think of copy this code sections)*extra
+
+let characters = ["abcdefghijklmnopqrstuvwxyz{|}~"]
 let password = document.getElementById("pass")
 
-password.value = "@&yghn#" 
+function generate(){
+    
+    
+    
+    for(let i = 0; i < length; i ++){
+        characters = (Math.random().toString(16).substring(2,length))
+        password.value = characters
+    }
+    
+    
+}
+
